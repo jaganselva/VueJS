f5991312ef19c7a8484bbcccbb556e3abf365774
Selva <jaganselva@gmail.com>
2020-08-16 13:31:53 -0700

    List rendering

--- VueMastery/IntroToVue3/main.js
+++ VueMastery/IntroToVue3/main.js
@@ -7,7 +7,14 @@ const app = Vue.createApp({
             url: 'https://google.com',
             inStock: false,
             inventory: 20,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'},
+            ],
+            sizes: ['Small', 'Large', 'Extra Large']
+
         }
     }
 })
