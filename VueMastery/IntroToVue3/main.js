0559a4f4b5e9cfce3aac8716de880355c98e2c52
Selva <jaganselva@gmail.com>
2020-08-16 11:23:38 -0700

    url, image v-bind

--- VueMastery/IntroToVue3/main.js
+++ VueMastery/IntroToVue3/main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            productDescription: 'Shoes description'
+            productDescription: 'Shoes description',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://google.com'
         }
     }
 })
