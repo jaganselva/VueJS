4af1abc35a78128053e43adf8c095d55e8cfa315
Selva <jaganselva@gmail.com>
2020-08-16 15:42:19 -0700

    Event Handling

--- VueMastery/IntroToVue3/main.js
+++ VueMastery/IntroToVue3/main.js
@@ -1,8 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            productDescription: 'Shoes description',
+            cart: 0,
+            product: 'Socks',
+            productDescription: 'Socks description',
             image: './assets/images/socks_green.jpg',
             url: 'https://google.com',
             inStock: false,
@@ -10,11 +11,25 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
             ],
-            sizes: ['Small', 'Large', 'Extra Large']
+            sizes: ['Small', 'Large', 'Extra Large'],
+            
 
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+
+        removeFromCart() {
+            this.cart -= 1
+        },
+
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
