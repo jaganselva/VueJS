368092625908511a1bf5ab59b5d5e0b7b5e4c448
Selva <jaganselva@gmail.com>
2020-08-16 16:52:42 -0700

    class & style binding

--- VueMastery/IntroToVue3/main.js
+++ VueMastery/IntroToVue3/main.js
@@ -7,7 +7,7 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://google.com',
             inStock: false,
-            inventory: 20,
+            inventory: 0,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
