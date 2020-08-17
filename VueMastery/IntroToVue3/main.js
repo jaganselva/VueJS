29cc98d5eb3f7b33b93f7d5d7ad9b19cb0a8d673
Selva <jaganselva@gmail.com>
2020-08-16 17:35:46 -0700

    computed properties

--- VueMastery/IntroToVue3/main.js
+++ VueMastery/IntroToVue3/main.js
@@ -3,16 +3,18 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
+            brand: 'VueMastery',
             productDescription: 'Socks description',
-            image: './assets/images/socks_green.jpg',
+            //image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             url: 'https://google.com',
-            inStock: false,
+            //inStock: false,
             inventory: 0,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ],
             sizes: ['Small', 'Large', 'Extra Large'],
             
@@ -28,8 +30,20 @@ const app = Vue.createApp({
             this.cart -= 1
         },
 
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
